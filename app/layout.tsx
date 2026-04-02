25e4ef91b7d663fa6ba8fcd1b4acb79434e1a517
Jayesh Karma <126589168+Jayesh-Karma@users.noreply.github.com>
2026-04-02 12:54:43 +0530

    Added GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ReactLenis from "lenis/react";
 import Navbar from "@/components/Hero/Navbar";
 import Footer from "@/components/Hero/Footer";
 import CalEmbed from "@/components/calendly/CadEmbed";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,9 +20,10 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Innovacio",
-  description: "A Upcoming AI-Powered Supply Chain Management Platform",
+  title: "Innovacio Technologies",
+  description: "AI Supply Chain Intelligence",
 };
+
 
 export default function RootLayout({
   children,
@@ -33,7 +35,29 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     > 
+    <head>
+          <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P8S7PZV');
+            `,
+          }}
+        />
+    </head>
       <body className="min-h-full flex flex-col">
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P8S7PZV" height="0" width="0" style={{display:"none" ,visibility:"hidden"}}></iframe>
+          </noscript> 
         
     <ReactLenis root>
       <GSAPProvider>
